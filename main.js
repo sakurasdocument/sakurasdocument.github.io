08038f5cbfb414abb2903cf14c7b062cde7dae7e
sakurasdocument <90955643+sakurasdocument@users.noreply.github.com>
2023-08-20 13:18:55 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -24,9 +24,10 @@ function initMap() {
 
         /*↓敵を出現させるメソッド*/
         class Enemy {
-            constructor(p1, p2) {
+            constructor(p1, p2, p3) {
                 this.lat = p1;
                 this.lng = p2;
+                this.img = p3;
                 /*↓クラスの中でメソッドを発火させなければならない*/
                 let enemy = this.createEnemy();
             }
@@ -36,7 +37,7 @@ function initMap() {
                     position: {lat: this.lat, lng: this.lng},
                     map: map,
                     icon: {
-                        url: 'shuma-stand.GIF',
+                        url: this.img,
                         scaledSize: new google.maps.Size(120, 120)
                     },
     
@@ -47,38 +48,46 @@ function initMap() {
 
 
         let enemys = [];
+        let enemyStock = [];
+        enemyStock = enemyStock.concat(JSON.parse(localStorage.getItem('enemyStock')) || []);
         enemys = enemys.concat(JSON.parse(localStorage.getItem('enemys')) || []);
         
-        for(i = 0; i < enemys.length; i++) {
+       /* for(i = 0; i < enemys.length; i++) {
             new Enemy(enemys[i].lat, enemys[i].lng);
         }
+        */
         
         function pushEnemy() {
             function enemyPush() {
                 if(enemys.length > 4) {
                     return;
                 }
+                
 
                 const source = [
                     {
                         name: '五千石球場',
                         lat: 36.41194,
-                        lng: 139.89193
+                        lng: 139.89193,
+                        img: 'norimaro-stand.GIF',
                     },
                     {
                         name: '清原南公園',
                         lat: 36.53694,
-                        lng: 139.98688
+                        lng: 139.98688,
+                        img: 'norimaro-stand.GIF',
                     },
                     {
                         name: '市貝町北運動場',
                         lat: 36.58048,
-                        lng: 140.11979
+                        lng: 140.11979,
+                        img: 'shuma-stand.GIF',
                     },
                     {
                         name: '生きがいの森',
                         lat: 36.59720,
-                        lng: 139.76889
+                        lng: 139.76889,
+                        img: 'shuma-stand.GIF',
                     },
                 ];
                 const n = Math.floor(Math.random() * source.length);
@@ -90,34 +99,66 @@ function initMap() {
         let elapsedTime = 0;
         elapsedTime = JSON.parse(localStorage.getItem('elapsedTime'));
 
-        const d =Date.now();
+        let d =Date.now();
         let startTime = d - elapsedTime;
-        
-        localStorage.setItem('elapsedTime', JSON.stringify(d));
+        elapsedTime = Date.now();
+        localStorage.setItem('elapsedTime', JSON.stringify(elapsedTime));
+        let s = Math.floor(startTime/10000);
 
+
+
+        console.log(s);
         console.log(startTime);
         console.log(elapsedTime);
+        console.log(sakuraLat, sakuraLng);
 
 
-
+        for(let t = 0; t < s; t++) {
             for(i = 0; i < enemys.length; i++) {
-                if(enemys[i].lat < sakuraLat) {
-                    enemys[i].lat = enemys[i].lat + (0.001 + (startTime / 1000));
+                if(enemys[i].lat < sakuraLat && enemys[i].lng < sakuraLng) {
+                    enemys[i].lat = enemys[i].lat + (0.01 * s);
+                    enemys[i].lng = enemys[i].lng + (0.01 * s);
+                    if(enemys[i].lat > sakuraLat && enemys[i].lng > sakuraLng){
+                        enemyStock.push(enemys.splice(i, 1)[0]);
+                    }
+                }else
+                if(enemys[i].lat < sakuraLat && enemys[i].lng > sakuraLng) {
+                    enemys[i].lat = enemys[i].lat + (0.01 * s);
+                    enemys[i].lng = enemys[i].lng - (0.01 * s); 
+                    if(enemys[i].lat > sakuraLat && enemys[i].lng < sakuraLng){
+                        enemyStock.push(enemys.splice(i, 1)[0]);
+                    }  
+                }else
+
+                if(enemys[i].lat > sakuraLat && enemys[i].lng < sakuraLng) {
+                    enemys[i].lat = enemys[i].lat - (0.01 * s);
+                    enemys[i].lng = enemys[i].lng + (0.01 * s); 
+                    if(enemys[i].lat < sakuraLat && enemys[i].lng > sakuraLng){
+                        enemyStock.push(enemys.splice(i, 1)[0]);
+                    }
+                }else
+                if(enemys[i].lat > sakuraLat && enemys[i].lng > sakuraLng) {
+                    enemys[i].lat = enemys[i].lat - (0.01 * s);
+                    enemys[i].lng = enemys[i].lng - (0.01 * s); 
+                    if(enemys[i].lat < sakuraLat && enemys[i].lng < sakuraLng){
+                        enemyStock.push(enemys.splice(i, 1)[0]);
+                    }
                 }
-                if(enemys[i].lat > sakuraLat) {
-                    enemys[i].lat = enemys[i].lat - (0.001 + (startTime / 1000));
-                }
-                if(enemys[i].lng < sakuraLng) {
-                    enemys[i].lng = enemys[i].lng + (0.001 + (startTime / 1000));
-                }
-                if(enemys[i].lng > sakuraLng) {
-                    enemys[i].lng = enemys[i].lng - (0.001 + (startTime / 1000));
-                }
+
+                
+
+
+
             }
+        }
+
+            console.log(enemyStock);
+            console.log(enemyStock.length);
 
 
 
             localStorage.setItem("enemys", JSON.stringify(enemys));
+            localStorage.setItem('enemyStock', JSON.stringify(enemyStock));
          
         /*   
         let elapsedTime = 0;
@@ -131,15 +172,52 @@ function initMap() {
         console.log(startTime);
         console.log(elapsedTime);
         */
+        let sakuralife;
+
+        sakuralife = 500;
+
+        let hp = document.getElementById('hp');
+        hp.style.width = sakuralife + 'px';
+
+        const waite = document.getElementById('waite');
+        waite.textContent = 'お待ちかね数♡:' + enemyStock.length;
+
+
+        const enemysImg = [
+            'shuma-stand.GIF',
+            'norimaro-stand.GIF'
+        ];
+
+        for(let i = 0; i < enemyStock.length; i++) {
+            const img = document.createElement('img');
+            img.src = enemyStock[i].img;
+
+            const li = document.createElement('li');
+            li.appendChild(img);
+
+            document.querySelector('.enemyStock').appendChild(li);
+        }
+        
+
 
             setTimeout(() => {
                 pushEnemy();
-            },1000);
+            },10000);
             console.log(enemys);
             console.log(enemys.length);
         }
 
         pushEnemy();
+
+        for(i = 0; i < enemys.length; i++) {
+            new Enemy(enemys[i].lat, enemys[i].lng, enemys[i].img);
+        }
+
+        
+        
+
+
+        
 
         /*↓TODO*/
         let vm = new Vue({
@@ -199,6 +277,8 @@ function initMap() {
         });  
     }
 
+    
+
     function fail(error) {
         window.alert('位置情報取得できず');
     }
@@ -206,27 +286,12 @@ function initMap() {
     navigator.geolocation.getCurrentPosition(success, fail);
 }
 
-let sakuralife;
 
-sakuralife = 500;
 
-let hp = document.getElementById('hp');
-hp.style.width = sakuralife + 'px';
 
-const enemysImg = [
-    'shuma-stand.GIF',
-    'norimaro-stand.GIF'
-];
 
-enemysImg.forEach(image => {
-    const img = document.createElement('img');
-    img.src = image;
 
-    const li = document.createElement('li');
-    li.appendChild(img);
 
-    document.querySelector('.enemyStock').appendChild(li);
-});
 
 
 
